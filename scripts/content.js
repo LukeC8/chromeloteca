c794663c962fa7ea2c5a39b6dfba52319be44267
Lucas Correa <lucas.correa@gmx.com>
2019-08-04 18:54:11 -0300

    Add simple script documentation
    Script purposes and actions described

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,8 @@
-//number id
-console.log('extension injected script');
-
+/*
+ * The inject.js script needs to be loaded into the page to to have access to the angular enviroment. The content script so inserts inject.js script inside loaded page and remove  the script from the page after load the message listener.
+ *
+ *
+ */
 let s = document.createElement('script');
 
 s.src = chrome.extension.getURL('scripts/inject.js');
